cb85c6bfe280f95956f861d33ad3e33d4ea57549
Motko <77466238+Motko222@users.noreply.github.com>
2024-09-25 18:26:11 +0200

    Create migrate.sh

---
+++ migrate.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+path=$(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd)
+folder=$(echo $path | awk -F/ '{print $NF}')
+
+read -p "New id (e.g. nillion-1) ?" id
+
+cd $path
+./stop.sh
+
+cd ~
+mv nillion $id 
+cp -r ~/$id/accuser ~/$id/verifier
+rm -r ~/scripts/nillion
+
+cd ~/scripts
+git clone https://github.com/Motko222/nillion-x
+mv nillion-x $id
+cd $id
+chmod +x *.sh
+
